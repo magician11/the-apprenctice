c792de0260863d40407267fb09db93d3087e6843
Andrew Golightly <support@andrewgolightly.com>
2016-10-01 03:11:08 +1300

    I have it typing to me

--- src/components/the-apprentice.jsx
+++ src/components/the-apprentice.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import TypeWriter from 'react-typewriter';
 
 import styling from '../styling/main.scss';
 
 class TheApprentice extends Component {
   render() {
-    const content = <p>Hello Andrew</p>;
+    const content = <TypeWriter typing={1}>Hello Andrew!</TypeWriter>;
 
     return (
       <div className={styling['the-apprentice']}>
-        {content}
+        <h1>{content}</h1>
       </div>
     );
   }
