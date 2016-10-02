4d2a02250f37dede98a7f487e92407270d786b38
Andrew Golightly <support@andrewgolightly.com>
2016-10-02 17:35:37 +1300

    added speech recognition

--- src/components/the-apprentice.jsx
+++ src/components/the-apprentice.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Typist from 'react-typist';
+import annyang from 'annyang';
 
 import styling from '../styling/main.scss';
 
@@ -10,9 +11,17 @@ class TheApprentice extends Component {
       thoughts: props.initialThoughts,
       currentThought: '',
       typing: true,
+      error: '',
     };
 
-    this.think = this.think.bind(this);
+    if (annyang) {
+      this.think = this.think.bind(this);
+      this.listen = this.listen.bind(this);
+      this.addThought = this.addThought.bind(this);
+      this.listen();
+    } else {
+      this.state.error = 'You need a different browser to interact with me. Try Google Chrome.';
+    }
   }
 
   think() {
@@ -28,15 +37,41 @@ class TheApprentice extends Component {
     }
   }
 
+  addThought(thought) {
+    console.log(`Adding thought: ${thought}`);
+    this.setState({
+      thoughts: this.state.thoughts.concat(thought),
+    });
+
+    this.think();
+  }
+
+  listen() {
+    const commands = {
+      'pretty good thanks': () => this.addThought('Awesome!'),
+      'repeat after me *sentence': sentence => this.addThought(sentence),
+    };
+
+    annyang.addCommands(commands);
+    annyang.start();
+    annyang.debug(true);
+  }
+
   render() {
+    console.log(this.state);
+    const { currentThought, error, typing } = this.state;
     let text;
-    if (this.state.typing) {
+
+    if (error) {
+      text = error;
+    } else if (typing) {
       const cursor = {
         show: false,
       };
-      text = <Typist onTypingDone={this.think} cursor={cursor}>{this.state.currentThought}</Typist>;
+      console.log(`About to type out ${currentThought}`);
+      text = <Typist onTypingDone={this.think} cursor={cursor}>{currentThought}</Typist>;
     } else {
-      text = this.state.currentThought;
+      text = currentThought;
     }
     return (
       <div className={styling['the-apprentice']}>
--- src/index.jsx
+++ src/index.jsx
@@ -5,5 +5,5 @@ import TheApprentice from './components/the-apprentice';
 // remove margins
 document.body.style.margin = 0;
 
-const initialThoughts = ['Hello Andrew', 'Let\'s chat', 'You can ask me anything...'];
+const initialThoughts = ['Hello Andrew', 'How are you doing today?'];
 ReactDOM.render(<TheApprentice initialThoughts={initialThoughts} />, document.getElementById('app'));
